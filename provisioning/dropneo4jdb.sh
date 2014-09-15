5659feeb59a3b4abc02c617bb5272b1bcedd52f0
Ramon Maria Gallart <rgallart@ramagaes.com>
2014-09-15 10:04:40 +0200

    Added script to drop Neo4j DB

---
+++ provisioning/dropneo4jdb.sh
@@ -0,0 +1,4 @@
+sudo service neo4j-service stop
+sudo rm -rf /var/lib/neo4j/data/graph.db
+sudo service neo4j-service start
+
--- provisioning/vagrant_script.sh
+++ provisioning/vagrant_script.sh
@@ -2,3 +2,6 @@ cd ~
 virtualenv 4dlife-env
 cd /vagrant/
 source /home/vagrant/4dlife-env/bin/activate && pip install -r requirements.txt
+mkdir -p /home/vagrant/bin/
+cp /vagrant/provisioning/dropneo4jdb.sh /home/vagrant/bin/dropneo4jdb.sh
+chmod 744 /home/vagrant/bin/dropneo4jdb.sh
